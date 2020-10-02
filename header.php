d98ef3b6c437d8c65e47ac7a6bae609ee2cfe97b
devyni <martynas.darskus@me.com>
2020-10-02 20:05:48 +0300

    Add header.php

---
+++ header.php
@@ -0,0 +1,19 @@
+<style>
+
+header {
+
+    height: 5%;
+    width: 100%;
+    background-color: grey;
+}
+</style>
+
+<header>
+
+<a href="./index.php">home</a>
+<a href="./about.php">about</a> 
+<a href="./news.php">news</a> 
+<a href="./contacts.php">contacts</a> 
+
+
+</header>
